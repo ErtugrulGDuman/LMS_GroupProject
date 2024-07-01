f3ce6b073a12cae50f97829fcdd14839bbeb92f7
Ertuğrul Gökay Duman <ertugrulgokayduman@gmail.com>
2024-07-01 22:21:09 +0300

    Folder structure, NuGet packages, Generic repository

---
+++ EntityLayer/Conrete/Forum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProject.EntityLayer.Conrete
+{
+    public class Forum
+    {
+        public int ForumId { get; set; }
+        public string GeneralQuestions { get; set; }
+        public string LessonQuestions { get; set; }
+        public string ScoreBoard { get; set; }
+    }
+}
---
+++ EntityLayer/Conrete/StudentCourses.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupProject.EntityLayer.Conrete
+{
+    public class StudentCourses
+    {
+        public int StudentCoursesId { get; set; }
+    }
+}
