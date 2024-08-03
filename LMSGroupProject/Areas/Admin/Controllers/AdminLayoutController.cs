0a1c022398bb9626cc7e40c42df43454d7e61300
tuba balkan <tubabalkan165@gmail.com>
2024-08-04 00:26:46 +0300

    Admin area controllerlar eklendi

---
+++ LMSGroupProject/Areas/Admin/Controllers/AdminLayoutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LMSGroupProject.Areas.Admin.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        [Area("Admin")]
+        [Route("Admin/AdminLayout/Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LMSGroupProject/Areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LMSGroupProject.Areas.Admin.Controllers
+{
+    public class ContactController : Controller
+    {
+        [Area("Admin")]
+        [Route("Admin/Contact/ContactList")]
+        public IActionResult ContactList()
+        {
+            return View();
+        }
+    }
+}
